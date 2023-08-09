641595a3d0f1e6ad37cd693ec9f3f348eed64b88
Kae Uchoa <kaeuchoas@gmail.com>
2023-08-09 11:08:31 -0300

    feat: add styles to position image, base text and icon

--- src/packages/PollsList/PollListItem/index.tsx
+++ src/packages/PollsList/PollListItem/index.tsx
@@ -1,6 +1,9 @@
 // PollListItem.tsx
-
 import React from 'react';
+import styles from './css/styles.module.css';
+import thumbUp from './assets/imgs/thumbs-up.svg';
+import thumbDown from './assets/imgs/thumbs-down.svg';
+import classNames from 'classnames';
 
 export type ThumbsIcon = 'thumb-up' | 'thumb-down';
 interface PollListItemProps {
@@ -31,30 +34,47 @@ const PollListItem: React.FC<PollListItemProps> = ({
   onVoteClick,
 }) => {
   return (
-    <div className="poll-list-item">
-      <div className="icon">{icon === 'thumb-up' ? '👍' : '👎'}</div>
+    <div className={styles["poll-list-item"]}>
       <img src={imageUrl} alt="" aria-hidden="true" />
-      <h1>{title}</h1>
-      <p className="short-description">{description}</p>
-      <p>{`Open for: ${openDuration} | Category: ${category}`}</p>
-      <div className="voting-buttons">
-        <button className="thumb-up-button" onClick={onThumbUpClick}>
-          Thumb Up
+      <div className={styles['content']}>
+        <div className={styles["header"]}>
+          <ResultIcon icon={icon} />
+          <h1>{title}</h1>
+        </div>
+        <p className="short-description">{description}</p>
+        <p>{`Open for: ${openDuration} | Category: ${category}`}</p>
+        <div className="voting-buttons">
+          <button className="thumb-up-button" onClick={onThumbUpClick}>
+            Thumb Up
+          </button>
+          <button className="thumb-down-button" onClick={onThumbDownClick}>
+            Thumb Down
+          </button>
+        </div>
+        <button className="vote-button" onClick={onVoteClick}>
+          Vote
         </button>
-        <button className="thumb-down-button" onClick={onThumbDownClick}>
-          Thumb Down
-        </button>
+        <div className="result-bar" >
+          <div className="result-thumb-up" style={{ width: `${(thumbsUpCount / (thumbsUpCount + thumbsDownCount)) * 100}%` }} />
+          <div className="result-thumb-down" style={{ width: `${(thumbsDownCount / (thumbsUpCount + thumbsDownCount)) * 100}%` }} />
+        </div>
+        <p className="result-count">{`${thumbsUpCount}% Thumb Up | ${thumbsDownCount}% Thumb Down`}</p>
       </div>
-      <button className="vote-button" onClick={onVoteClick}>
-        Vote
-      </button>
-      <div className="result-bar" >
-        <div className="result-thumb-up" style={{ width: `${(thumbsUpCount / (thumbsUpCount + thumbsDownCount)) * 100}%` }} />
-        <div className="result-thumb-down" style={{ width: `${(thumbsDownCount / (thumbsUpCount + thumbsDownCount)) * 100}%` }} />
-      </div>
-      <p className="result-count">{`${thumbsUpCount}% Thumb Up | ${thumbsDownCount}% Thumb Down`}</p>
     </div>
   );
 };
+
+const ResultIcon = ({ icon }: { icon: ThumbsIcon }) => {
+  const isThumbUp = icon === 'thumb-up';
+  return (
+    <span className={classNames({
+      [styles['result-icon']]: true,
+      [styles['result-icon--positive']]: isThumbUp,
+      [styles['result-icon--negative']]: !isThumbUp,
+    })}>
+      <img src={isThumbUp ? thumbUp : thumbDown} alt={isThumbUp ? 'thumbs up' : 'thumbs down'} />
+    </span>
+  )
+}
 
 export default PollListItem;
