25fd60dc4ec40a92757f4db55c82554301ea2748
Kae Uchoa <kaeuchoas@gmail.com>
2023-08-17 16:18:02 -0300

    feat: add formatTimePeriod

--- src/packages/PollsList/shared/spec/formatTimePeriod.spec.ts
+++ src/packages/PollsList/shared/spec/formatTimePeriod.spec.ts
@@ -1,20 +1,4 @@
-function formatTimePeriod(days: number): string {
-  if (days < 1) {
-    return '0 days';
-  }
-
-  if (days < 30) {
-    return `${days} ${days === 1 ? 'day' : 'days'}`;
-  }
-
-  if (days < 365) {
-    const months = Math.floor(days / 30);
-    return `${months} ${months === 1 ? 'month' : 'months'}`;
-  }
-
-  const years = Math.floor(days / 365);
-  return `${years} ${years === 1 ? 'year' : 'years'}`;
-}
+import { formatTimePeriod } from '../formatTimePeriod';
 
 describe('formatTimePeriod function', () => {
   it('should return "0 days" for input less than 1', () => {
