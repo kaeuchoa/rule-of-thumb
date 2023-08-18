c818b98d1081e1895f6744a92772bc7ea033ad90
Kae Uchoa <kaeuchoas@gmail.com>
2023-08-18 18:38:23 -0300

    test: add tests to PollCard

--- src/packages/PollCard/spec/index.spec.tsx
+++ src/packages/PollCard/spec/index.spec.tsx
@@ -1,22 +1,66 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import PollCard from '..';
+import { render, screen, fireEvent } from '@testing-library/react';
+import PollCard from '../.';
 
-
-describe('PollCard component', () => {
+describe('PollCard', () => {
   it('renders all elements correctly', () => {
-    const primaryTitle = 'Celebrity name';
-    const description = 'Some description';
-    const altText = 'hey this is my alt text';
-    
-    render(<PollCard primaryTitle={primaryTitle} description={description} onThumbDownClick={jest.fn()} onThumbUpClick={jest.fn()} />)
+    const onThumbUpClickMock = jest.fn();
+    const onThumbDownClickMock = jest.fn();
 
-    expect(screen.getByText("What's your opinion on")).toBeInTheDocument();
-    expect(screen.getByText(primaryTitle)).toBeInTheDocument();
-    expect(screen.getByText(description)).toBeInTheDocument();
-    expect(screen.getByText("What's your veridict?")).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Thumb Up' })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Thumb Down' })).toBeInTheDocument();
-    expect(screen.getByAltText(altText)).toBeInTheDocument();
+    render(
+      <PollCard
+        primaryTitle="Sample Title"
+        description="Sample Description"
+        onThumbUpClick={onThumbUpClickMock}
+        onThumbDownClick={onThumbDownClickMock}
+      />
+    );
+
+    const titleElement = screen.getByText('Sample Title');
+    const descriptionElement = screen.getByText('Sample Description');
+    const thumbsUpButton = screen.getByLabelText('thumbs up');
+    const thumbsDownButton = screen.getByLabelText('thumbs down');
+
+    expect(titleElement).toBeInTheDocument();
+    expect(descriptionElement).toBeInTheDocument();
+    expect(thumbsUpButton).toBeInTheDocument();
+    expect(thumbsDownButton).toBeInTheDocument();
+  });
+
+  it('handles "thumbs up" button click', () => {
+    const onThumbUpClickMock = jest.fn();
+    const onThumbDownClickMock = jest.fn();
+
+    render(
+      <PollCard
+        primaryTitle="Sample Title"
+        description="Sample Description"
+        onThumbUpClick={onThumbUpClickMock}
+        onThumbDownClick={onThumbDownClickMock}
+      />
+    );
+
+    const thumbsUpButton = screen.getByLabelText('thumbs up');
+    fireEvent.click(thumbsUpButton);
+
+    expect(onThumbUpClickMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('handles "thumbs down" button click', () => {
+    const onThumbUpClickMock = jest.fn();
+    const onThumbDownClickMock = jest.fn();
+
+    render(
+      <PollCard
+        primaryTitle="Sample Title"
+        description="Sample Description"
+        onThumbUpClick={onThumbUpClickMock}
+        onThumbDownClick={onThumbDownClickMock}
+      />
+    );
+
+    const thumbsDownButton = screen.getByLabelText('thumbs down');
+    fireEvent.click(thumbsDownButton);
+
+    expect(onThumbDownClickMock).toHaveBeenCalledTimes(1);
   });
 });
