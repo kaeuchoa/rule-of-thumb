2bbd626723e9bdb083c314924e878f5b084a6fc7
Kae Uchoa <kaeuchoas@gmail.com>
2023-08-18 16:54:27 -0300

    feat: add slider

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,4 +1,6 @@
 import PollListItem from '@rule-of-thumb/poll-list-item';
+import Flickity from 'react-flickity-component'
+import { useMediaQuery } from 'react-responsive'
 import CTABanner from './CTABanner';
 import SpeakOutBanner from './SpeakOutBanner';
 import { usePolls } from '../data/PollService';
@@ -7,10 +9,11 @@ import { VoteType } from '../shared/types';
 import LayoutToggle from './LayoutToggle';
 import styles from './styles.module.css'
 
+
 const Main = () => {
   const { data: polls, isLoading } = usePolls();
-  const { handleVoteClick, setVote } = useVoteHandler()
-
+  const { handleVoteClick, setVote } = useVoteHandler();
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
   if (isLoading) {
     return <>loading....</>
   }
@@ -25,26 +28,60 @@ const Main = () => {
       </section>
       <section>
         <h2>Previous Rulings</h2>
-        <LayoutToggle>
-          {polls.map(({ id, celebrity, openingDate, resultIcon }) => (
-            <li>
-              <PollListItem
-                key={id}
-                category={celebrity.category}
-                description={celebrity.description}
-                icon={resultIcon}
-                imageUrl={celebrity.picture}
-                openDuration={openingDate}
-                thumbsDownCount={celebrity.votes.negative}
-                thumbsUpCount={celebrity.votes.positive}
-                title={celebrity.name}
-                onThumbDownClick={() => setVote({ pollId: id, vote: VoteType.negative })}
-                onThumbUpClick={() => setVote({ pollId: id, vote: VoteType.positive })}
-                onVoteClick={handleVoteClick}
-              />
-            </li>
-          ))}
-        </LayoutToggle>
+        {isMobile && (
+          <Flickity
+            elementType={'div'}
+            options={{
+              prevNextButtons: false
+            }}
+            disableImagesLoaded={false}
+            reloadOnUpdate
+            static
+          >
+            {polls.map(({ id, celebrity, openingDate, resultIcon }) => (
+              <li style={{ marginRight: '10px' }}>
+                <PollListItem
+                  key={id}
+                  category={celebrity.category}
+                  description={celebrity.description}
+                  icon={resultIcon}
+                  imageUrl={celebrity.picture}
+                  openDuration={openingDate}
+                  thumbsDownCount={celebrity.votes.negative}
+                  thumbsUpCount={celebrity.votes.positive}
+                  title={celebrity.name}
+                  onThumbDownClick={() => setVote({ pollId: id, vote: VoteType.negative })}
+                  onThumbUpClick={() => setVote({ pollId: id, vote: VoteType.positive })}
+                  onVoteClick={handleVoteClick}
+                  isMobileView={true}
+                />
+              </li>
+            ))}
+          </Flickity>
+        )}
+        {!isMobile && (
+          <LayoutToggle>
+            {layout => polls.map(({ id, celebrity, openingDate, resultIcon }) => (
+              <li>
+                <PollListItem
+                  key={id}
+                  category={celebrity.category}
+                  description={celebrity.description}
+                  icon={resultIcon}
+                  imageUrl={celebrity.picture}
+                  openDuration={openingDate}
+                  thumbsDownCount={celebrity.votes.negative}
+                  thumbsUpCount={celebrity.votes.positive}
+                  title={celebrity.name}
+                  onThumbDownClick={() => setVote({ pollId: id, vote: VoteType.negative })}
+                  onThumbUpClick={() => setVote({ pollId: id, vote: VoteType.positive })}
+                  onVoteClick={handleVoteClick}
+                  isMobileView={layout === 'grid'}
+                />
+              </li>
+            ))}
+          </LayoutToggle>
+        )}
       </section>
       <section>
         <CTABanner />
